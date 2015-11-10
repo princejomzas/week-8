53c95ea3543c49d082c2d52581bbe6afc906bc7f
princejomzas <princejomza@gmail.com>
2015-11-10 19:52:46 +0700

    New View implement

---
+++ TwoZeroFourEightScoreView.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace twozerofoureight
+{
+    partial class TwoZeroFourEightScoreView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score View";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score";
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Location = new System.Drawing.Point(194, 48);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(13, 13);
+            this.lblscore.TabIndex = 2;
+            this.lblscore.Text = "0";
+            // 
+            // TwoZeroFourEightScoreView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 94);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TwoZeroFourEightScoreView";
+            this.Text = "TwoZeroFourEightScoreView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblscore;
+    }
+}
---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form, View
+    {       
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+        }
+        public void Notify(Model m)
+        {
+            UpdateScoreView(((TwoZeroFourEightModel)m).total());
+        }
+        public void UpdateScoreView(int sum)
+        {
+            lblscore.Text = Convert.ToString(sum);
+        }
+    }
+}
